307966b7d456956a230694f4ad4728bc7b85372c
tedhz <ted.h.zhang@gmail.com>
2022-05-23 15:26:48 -0400

    Added content to Projects

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom'
 import Home from './pages/home/Home';
 import About from './pages/about/About';
-import Projects from './pages/Projects';
+import Projects from './pages/projects/Projects';
 import Contact from './pages/contact/Contact';
 import Resume from './pages/resume/Resume';
 
---
+++ src/pages/projects/Projects.js
@@ -0,0 +1,126 @@
+import './Projects.scss';
+
+function Projects () {
+    return (
+        <>
+            <div className='projects-container'>
+                <div className='text-zone-projects'>
+                    <h1 className='title-projects'>Projects</h1>
+                    <h2 className='projects-description'>
+                        Some of my professional and personal projects.
+                    </h2>
+                </div>
+                <div className='projects-content'>
+                    <div className='project'>
+                        <div className='project-background'>
+                            <div className='project-details'>
+                                <ul className='skills-used'>
+                                    <li className='skill'>
+                                        React.js    
+                                    </li>
+                                    <li className='skill'>
+                                        SASS    
+                                    </li>
+                                    <li className='skill'>
+                                        Javascript    
+                                    </li>    
+                                </ul>
+                                <div className='project-description'>
+                                    The website you are currently on is my personal website.
+                                    Built from the ground up using React.js and SASS, 
+                                    this portofolio site was my spearhead into front-end 
+                                    development, and ignited an interest that I intend to 
+                                    continue pursuing. Check out all the features by clicking
+                                    around!
+                                </div>
+                                <div className='code-button-container'>
+                                    <a href='https://github.com/tedhz/ted-zhang-website' target="_blank" 
+                                    className='code-button' >
+                                        Get Code
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='project-name-container'>
+                            <div className='project-name'>Personal Website</div>
+                        </div>
+                    </div>
+
+                    <div className='project'>
+                        <div className='project-background'>
+                            <div className='project-details'>
+                                <ul className='skills-used'>
+                                    <li className='skill'>
+                                        Tensorflow    
+                                    </li>
+                                    <li className='skill'>
+                                        Keras    
+                                    </li>
+                                    <li className='skill'>
+                                        Pandas    
+                                    </li>
+                                    <li className='skill'>
+                                        Tkinter    
+                                    </li>
+                                    <li className='skill'>
+                                        Python    
+                                    </li>
+                                    <li className='skill'>
+                                        AI/ML    
+                                    </li>    
+                                </ul>
+                                <div className='project-description'>
+                                    This project was built completely by me from scratch
+                                    during my time as an Artificial Intelligence
+                                    Intern at AMD. In short, this AI aims to 
+                                    identify and diagnose intracranial hemorrhages
+                                    in brain scans, which it is able to do with 94%
+                                    validation accuracy. It is trained on over 700000
+                                    medical images and uses transfer learning on an InceptionV3 
+                                    base. It also comes complete with both a CLI and GUI
+                                    for a smoother user experience.
+                                </div>
+                            </div>
+                        </div>
+                        <div className='project-name-container'>
+                            <div className='project-name'>medCNN</div>
+                        </div>
+                    </div>
+
+                    <div className='project'>
+                        <div className='project-background'>
+                            <div className='project-details'>
+                                <ul className='skills-used'>
+                                    <li className='skill'>
+                                        Java    
+                                    </li>
+                                    <li className='skill'>
+                                        ArrayList    
+                                    </li>
+                                    <li className='skill'>
+                                        Java AWT    
+                                    </li>    
+                                </ul>
+                                <div className='project-description'>
+                                    This project was a recreation of the board game
+                                    "Mastermind", which came complete with both a CLI
+                                    and GUI option to play with. The game came with
+                                    player vs. player as well as player vs. AI capabilites,
+                                    with the AI taking on three levels of difficulty. The
+                                    most difficult level leveraged an algorithmic artificial
+                                    intelligence that can beat nearly any
+                                    human player. 
+                                </div>
+                            </div>
+                        </div>
+                        <div className='project-name-container'>
+                            <div className='project-name'>Mastermind Game</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Projects;
